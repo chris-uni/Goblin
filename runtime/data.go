6d315bbdc400288697305b7d61baf38f853f0988
chris-uni <chris.wing1@btinternet.com>
2024-11-28 20:05:36 +0000

    Added data.size, added data.size tests

--- runtime/data.go
+++ runtime/data.go
@@ -19,7 +19,7 @@ var Data = Namespace{
 		},
 		"size": {
 			Type: "NativeFn",
-			Call: sprintf,
+			Call: size,
 		},
 	},
 }
@@ -41,8 +41,34 @@ var push FunctionCall = func(args []RuntimeValue, env Environment) (RuntimeValue
 
 	// The value we want to push into the array.
 	value := args[1]
-
 	*arr.Value = append(*arr.Value, value)
 
 	return MK_NULL(), nil
+}
+
+// size, returns the size of the array or map specified
+// data.size(a array), data.size(m map)
+var size FunctionCall = func(args []RuntimeValue, env Environment) (RuntimeValue, error) {
+
+	numArgs := len(args)
+	if numArgs != 1 {
+		return nil, fmt.Errorf("unexpected number of args for data.size, expected 1 got %v", numArgs)
+	}
+
+	a := args[0]
+	arr, isArr := a.(ArrayValue)
+	mapp, isMap := a.(MapValue)
+
+	if !isArr && !isMap {
+		return nil, fmt.Errorf("unexpected type provided for data.size, got %v", a)
+	}
+
+	size := 0
+	if isArr {
+		size = len(*arr.Value)
+	} else if isMap {
+		size = len(mapp.Value)
+	}
+
+	return MK_NUMBER(size), nil
 }
--- tests/data_test.go
+++ tests/data_test.go
@@ -63,3 +63,72 @@ func TestPush(t *testing.T) {
 		})
 	}
 }
+
+func TestSize(t *testing.T) {
+
+	// Setup the program env.
+	HarnessSetup()
+
+	var tests = []struct {
+		source      string
+		want        string
+		throwsError bool
+	}{
+		{`using "data";
+		using "io";
+		let arr = [1, 2, 3, 4, 5];
+		let sArr = data.size(arr);
+		io.print(sArr);`, "5", false},
+		{`using "data";
+		using "io";
+		let map = {
+			"foo": 10,
+			"bar": 20,
+			"baz": 30,
+		};
+		let sMap = data.size(map);
+		io.print(sMap);`, "3", false},
+		{`using "data";
+		using "io";
+		let arrr = [];
+		let sArrr = data.size(arrr);
+		io.print(sArrr);`, "0", false},
+		{`using "data";
+		using "io";
+		let arrrr = [];
+		data.push(arrr, 1);
+		data.push(arrr, 2);
+		data.push(arrr, 3);
+		let sArrrr = data.size(arrr);
+		io.print(sArrrr);`, "3", false},
+	}
+
+	for _, tt := range tests {
+		testname := fmt.Sprintf("%v, %v", tt.source, tt.want)
+		t.Run(testname, func(t *testing.T) {
+
+			// Run the program.
+			_, err := program.Run(string(tt.source), env)
+
+			if !tt.throwsError {
+
+				// When tests aren't supposed to throw an error.
+				if err != nil {
+					t.Errorf(err.Error())
+				}
+
+				if output.String() != tt.want {
+					t.Errorf("expected `%v`, received `%v`", tt.want, output.String())
+				}
+			} else {
+
+				// When tests are supposed to throw an error.
+				if err.Error() != tt.want {
+					t.Errorf("expected `%v`, received `%v`", tt.want, err.Error())
+				}
+			}
+
+			FlushBuffer()
+		})
+	}
+}
