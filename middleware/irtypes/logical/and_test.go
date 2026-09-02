b6d07b43a582a861bb89efae7c55accfaa79e239
Mr Cdept <mrcdept@Mrs-MacBook-Pro.local>
2026-09-02 16:02:17 +0100

    Adding logical opperand validation tests

---
+++ middleware/irtypes/logical/and_test.go
@@ -0,0 +1,92 @@
+package logical
+
+import (
+	"fmt"
+	"testing"
+
+	i "goblin.org/main/middleware/irtypes"
+)
+
+func Test_And_Expression_Invalid_Type_LHS_Number(t *testing.T) {
+
+	context := i.IRContext{}
+	irTemp := context.AllocateTemporary()
+
+	a := And{
+		Destination: irTemp,
+		Lhs:         i.IRNumber{Value: 10},
+		Rhs:         i.IRBoolean{Value: true},
+	}
+
+	out := a.Validate(&context)
+
+	want := "type error: and: operands of invalid type\n"
+	got := fmt.Sprintf("%v", out)
+
+	if got != want {
+		t.Errorf("\ngot %v\nwant %v\n", got, want)
+	}
+}
+
+func Test_And_Expression_Invalid_Type_LHS_String(t *testing.T) {
+
+	context := i.IRContext{}
+	irTemp := context.AllocateTemporary()
+
+	a := And{
+		Destination: irTemp,
+		Lhs:         i.IRString{Value: "Hello"},
+		Rhs:         i.IRNumber{Value: 10},
+	}
+
+	out := a.Validate(&context)
+
+	want := "type error: and: operands of invalid type\n"
+	got := fmt.Sprintf("%v", out)
+
+	if got != want {
+		t.Errorf("\ngot %v\nwant %v\n", got, want)
+	}
+}
+
+func Test_And_Expression_Invalid_Type_RHS_Number(t *testing.T) {
+
+	context := i.IRContext{}
+	irTemp := context.AllocateTemporary()
+
+	a := And{
+		Destination: irTemp,
+		Lhs:         i.IRBoolean{Value: true},
+		Rhs:         i.IRNumber{Value: 10},
+	}
+
+	out := a.Validate(&context)
+
+	want := "type error: and: operands of invalid type\n"
+	got := fmt.Sprintf("%v", out)
+
+	if got != want {
+		t.Errorf("\ngot %v\nwant %v\n", got, want)
+	}
+}
+
+func Test_And_Expression_Invalid_Type_RHS_String(t *testing.T) {
+
+	context := i.IRContext{}
+	irTemp := context.AllocateTemporary()
+
+	a := And{
+		Destination: irTemp,
+		Lhs:         i.IRBoolean{Value: true},
+		Rhs:         i.IRString{Value: "Hello"},
+	}
+
+	out := a.Validate(&context)
+
+	want := "type error: and: operands of invalid type\n"
+	got := fmt.Sprintf("%v", out)
+
+	if got != want {
+		t.Errorf("\ngot %v\nwant %v\n", got, want)
+	}
+}
--- middleware/irtypes/logical/or.go
+++ middleware/irtypes/logical/or.go
@@ -1,15 +1,35 @@
 package logical
 
-import i "goblin.org/main/middleware/irtypes"
+import (
+	"fmt"
+
+	i "goblin.org/main/middleware/irtypes"
+)
 
 type Or struct {
 	Destination i.IRTemporary
-	Val1        i.IRValue
-	Val2        i.IRValue
+	Lhs         i.IRValue
+	Rhs         i.IRValue
 }
 
 func (o *Or) Exec(context *i.IRContext) {}
 
 func (o *Or) Validate(context *i.IRContext) error {
+
+	// Does both the lhs and rhs of the command adhere to the commands rules?
+	lhsType, err := i.ResolveIRType(o.Lhs, context)
+	if err != nil {
+		return err
+	}
+
+	rhsType, err := i.ResolveIRType(o.Rhs, context)
+	if err != nil {
+		return err
+	}
+
+	if lhsType != i.IRTypeBoolean || rhsType != i.IRTypeBoolean {
+		return fmt.Errorf("type error: or: operands of invalid type\n")
+	}
+
 	return nil
 }
